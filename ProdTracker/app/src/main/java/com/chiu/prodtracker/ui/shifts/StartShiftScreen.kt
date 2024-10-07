c1756655d280fedd70f712a669ec8df37201b021
elpguillen <elpguillen@users.noreply.github.com>
2024-10-06 19:21:56 -0700

    Add Body of the StartShiftScreen in ProdTracker.

--- ProdTracker/app/src/main/java/com/chiu/prodtracker/ui/shifts/StartShiftScreen.kt
+++ ProdTracker/app/src/main/java/com/chiu/prodtracker/ui/shifts/StartShiftScreen.kt
@@ -1,2 +1,21 @@
 package com.chiu.prodtracker.ui.shifts
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StartShiftScreen() {
+
+}
+
+@Composable
+fun StartShiftBody() {
+    Row {
+        OutlinedButton(onClick = { }) {
+            Text(text = "Start Shift")
+        }
+    }
+}
