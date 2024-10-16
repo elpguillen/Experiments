5b2f6c641047e1478f8c58c5d3fa3eef42193672
elpguillen <elpguillen@users.noreply.github.com>
2024-10-15 18:29:59 -0700

    Add EndShiftScreen, EndShiftBody, and StartShiftBody to populate StartShiftScreen and EndShiftScreen.

--- ProdTracker/app/src/main/java/com/chiu/prodtracker/ui/shifts/EndShiftScreen.kt
+++ ProdTracker/app/src/main/java/com/chiu/prodtracker/ui/shifts/EndShiftScreen.kt
@@ -1,2 +1,20 @@
 package com.chiu.prodtracker.ui.shifts
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EndShiftScreen() {
+    EndShiftBody()
+}
+
+@Composable
+fun EndShiftBody() {
+    Row {
+        OutlinedButton(onClick = {}) {
+            Text(text = "End Shift")
+        }
+    }
+}
--- ProdTracker/app/src/main/java/com/chiu/prodtracker/ui/shifts/StartShiftScreen.kt
+++ ProdTracker/app/src/main/java/com/chiu/prodtracker/ui/shifts/StartShiftScreen.kt
@@ -8,7 +8,7 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun StartShiftScreen() {
-
+    StartShiftBody()
 }
 
 @Composable
