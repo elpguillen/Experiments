15e6f32be5ce4f635d926c0736f52104058f7683
elpguillen <elpguillen@users.noreply.github.com>
2024-10-20 17:37:42 -0700

    Create starting template for LunchShiftScreen.

--- ProdTracker/app/src/main/java/com/chiu/prodtracker/ui/shifts/LunchShiftScreen.kt
+++ ProdTracker/app/src/main/java/com/chiu/prodtracker/ui/shifts/LunchShiftScreen.kt
@@ -1,2 +1,13 @@
 package com.chiu.prodtracker.ui.shifts
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LunchShiftScreen() {
+    LunchShiftBody()
+}
+
+@Composable
+fun LunchShiftBody() {
+
+}
